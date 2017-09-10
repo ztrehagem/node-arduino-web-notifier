6c4b7fa35e76163f43b74104dd217755ecf85061
Shigeki Suwa <suwa.shigeki@gmail.com>
2017-09-10 14:26:45 +0700

    use EventEmitter

--- app/main.js
+++ app/main.js
@@ -1,7 +1,11 @@
+// -- imports == //
 const HttpServer = require('./server');
 const SocketServer = require('./socket');
 const SerialReceiver = require('./serial-receiver');
+const Utils = require('./utils');
 
+
+// -- main -- //
 var portName = process.argv[2];
 
 if (!portName) {
@@ -13,4 +17,14 @@ if (!portName) {
 var httpServer = HttpServer.init();
 var socketServer = SocketServer.init(httpServer);
 var serialReceiver = new SerialReceiver(portName, socketServer);
+
+serialReceiver.on('message', (message) => sendToSocket(message));
+
 httpServer.listen(8080);
+
+
+// -- functinos -- //
+function sendToSocket(message) {
+  socketServer.emit('message-from-arduino', message);
+  Utils.log('emitted to socket:', message);
+}
--- app/serial-receiver.js
+++ app/serial-receiver.js
@@ -1,19 +1,21 @@
 // -- imports -- //
+const EventEmitter = require('events');
 const SerialPort = require('serialport');
 const Utils = require('./utils');
 
-// -- class -- //
-module.exports = class SerialReceiver {
-  constructor(portName, socketServer) {
+// -- export class -- //
+module.exports = class SerialReceiver extends EventEmitter {
+  constructor(portName) {
+    super();
+    
     this.port = new SerialPort(portName, { baudRate: 9600 });
-    this.socketServer = socketServer;
     this.buf = new Buffer(0);
 
     this.port.on('data', (data) => this.receive(data));
   }
 
   receive(data) {
-    Utils.log('arduino says:', data.toString());
+    Utils.log('arduino said:', data.toString());
     this.pushBuf(data);
     this.checkBuf();
   }
@@ -22,22 +24,17 @@ module.exports = class SerialReceiver {
     this.buf = Buffer.concat([this.buf, buf]);
   }
 
-  checkBuf() {
-    if (this.buf.length >= 3) {
-      var sendData = this.shiftBuf(3);
-      this.emitToClient(sendData);
-    }
-  }
-
   shiftBuf(length) {
     var data = this.buf.slice(0, length);
     this.buf = this.buf.slice(length);
     return data;
   }
 
-  emitToClient(sendData) {
-    var str = sendData.toString();
-    this.socketServer.emit('message-from-arduino', str);
-    Utils.log('emitted to client:', str);
+  checkBuf() {
+    // check whether received 3 bytes from serial port
+    if (this.buf.length >= 3) {
+      var str = this.shiftBuf(3).toString();
+      this.emit('message', str);
+    }
   }
 }
