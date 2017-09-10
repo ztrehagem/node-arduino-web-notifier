a12102857f3cfbf58831d6eb1900d427af73e120
Shigeki Suwa <suwa.shigeki@gmail.com>
2017-09-10 12:10:01 +0700

    display message from arduino as HTML Content

--- public/index.js
+++ public/index.js
@@ -1,9 +1,25 @@
+function nowString() {
+  return new Date().toLocaleTimeString();
+}
+
+function createMessageListItem(message) {
+  var eListItem = document.createElement('li');
+  eListItem.innerText = '[' + nowString() + '] ' + message;
+  return eListItem;
+}
+
+function displayMessage(message) {
+  var eList = document.getElementById('messages');
+  var eListItem = createMessageListItem(message);
+  eList.appendChild(eListItem);
+}
+
 function initSocket(socket) {
   // catch the 'greeting' event from server
   socket.on('greeting', (data) => console.log(data));
 
   // catch the 'message-from-arduino' event from server
-  socket.on('message-from-arduino', (str) => console.log(str));
+  socket.on('message-from-arduino', (message) => displayMessage(message));
 
   // send the 'echo' event with text 'test1234' to server
   // and catch the response
